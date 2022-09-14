94567c4be69378f48dd75b1fe0d136704cf20896
SantyJR18 <norman.manzanares15129@est.uca.edu.ni>
2022-09-13 19:23:49 -0600

    Video de POO

---
+++ src/operacion/Principal.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operacion;
+
+import formularios.FrmPrueba;
+
+/**
+ *
+ * @author manza
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrmPrueba v = new FrmPrueba ();
+        v.setvisible(true);
+    }
+}
